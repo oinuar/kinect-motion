06bb729de8840688d73755fb5df06d16dc1d5921
Jussi Raunio <jussi.raunio@gmail.com>
2018-01-10 23:37:19 +0200

    Add project files.

---
+++ kinect-motion-server/Program.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.Kinect;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace KinectMotion
+{
+   class Program : IDisposable
+   {
+      KinectSensor Sensor { get; set; }
+
+      BodyFrameReader BodyFrameReader { get; set; }
+
+      BodyIndexFrameReader BodyIndexFrameReader { get; set; }
+
+      WebSocketServer Server { get; set; }
+
+      Queue<byte[]> Queue { get; set; }
+
+      JsonSerializerSettings SerializerSettings { get; set; }
+
+      Body[] Bodies { get; set; }
+
+      BodyIndex BodyIndex { get; set; }
+
+      Program()
+      {
+         Sensor = KinectSensor.GetDefault();
+         BodyFrameReader = Sensor.BodyFrameSource.OpenReader();
+         BodyIndexFrameReader = Sensor.BodyIndexFrameSource.OpenReader();
+         Queue = new Queue<byte[]>();
+         SerializerSettings = new JsonSerializerSettings
+         {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Culture = CultureInfo.InvariantCulture,
+            Formatting = Formatting.None
+         };
+
+         // Streaming server will listen 8521 port and use WebSockets.
+         Server = new WebSocketServer(8521);
+
+         var bodyIndexFrameDescription = Sensor.BodyIndexFrameSource.FrameDescription;
+
+         // Body index object that contains dimensions alongside with pixel data.
+         BodyIndex = new BodyIndex
+         {
+            Width = bodyIndexFrameDescription.Width,
+            Height = bodyIndexFrameDescription.Height,
+            Pixels = new byte[bodyIndexFrameDescription.Width * bodyIndexFrameDescription.Height]
+         };
+
+         // We are interested in these Kinect frames. Body Index Frame contains
+         // a raw body data. Body Frame contains the same data but in more structual
+         // form.
+         BodyIndexFrameReader.FrameArrived += BodyIndexFrameReader_FrameArrived;
+         BodyFrameReader.FrameArrived += BodyFrameReader_FrameArrived;
+
+         Server.ClientConnected += Server_ClientConnected;
+
+         // Start server and Kinect sensor.
+         Server.Start();
+         Sensor.Open();
+      }
+
+      private void Server_ClientConnected(object sender, ClientConnectionEventArgs e)
+      {
+         Console.WriteLine("Client connected. Currently there are {0} connection(s).", e.Connections);
+      }
+
+      static void Main(string[] args)
+      {
+         using (var program = new Program())
+         {
+            Console.WriteLine("Serving on {0} endpoint(s).", string.Join(", ", program.Server.Endpoints));
+
+            // Stream the data as long as the application is running.
+            program.Stream();
+         }
+      }
+
+      void Stream()
+      {
+         lock (Queue)
+         {
+
+            // Stream until the server is alive.
+            while (Server.KeepAlive())
+            {
+
+               // Apply frame updates in order they have arrived.
+               while (Queue.Count > 0)
+                  Server.Send(Queue.Dequeue()).Wait();
+
+               Monitor.Wait(Queue);
+            }
+         }
+      }
+
+      void BodyFrameReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+      {
+         using (var frame = e.FrameReference.AcquireFrame())
+         {
+            if (frame == null)
+               return;
+
+            // Create or refresh body container.
+            if (Bodies == null || frame.BodyCount != Bodies.Length)
+               Bodies = new Body[frame.BodyCount];
+
+            // Update the body data.
+            frame.GetAndRefreshBodyData(Bodies);
+         }
+
+         // Create JSON payload of the data.
+         var payload = ToJson(new Payload(Bodies, BodyIndex));
+
+         // Put data to send queue as soon as it arrives.
+         lock (Queue)
+         {
+            Queue.Enqueue(payload);
+            Monitor.Pulse(Queue);
+         }
+      }
+
+      void BodyIndexFrameReader_FrameArrived(object sender, BodyIndexFrameArrivedEventArgs e)
+      {
+         using (var frame = e.FrameReference.AcquireFrame())
+         {
+            if (frame == null)
+               return;
+
+            // Copy body index pixels to our memory. This itself won't update
+            // connected clients; instead we wait for parsed and analyzed
+            // body data to arrive from Kinect and update only then.
+            frame.CopyFrameDataToArray(BodyIndex.Pixels);
+         }
+      }
+
+      byte[] ToJson<T>(T value)
+      {
+         return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(value, SerializerSettings));
+      }
+
+      public void Dispose()
+      {
+         if (Server != null)
+            Server.Dispose();
+
+         if (BodyFrameReader != null)
+            BodyFrameReader.Dispose();
+
+         if (BodyIndexFrameReader != null)
+            BodyIndexFrameReader.Dispose();
+
+         if (Sensor != null)
+            Sensor.Close();
+
+         Server = null;
+         BodyFrameReader = null;
+         BodyIndexFrameReader = null;
+         Sensor = null;
+      }
+   }
+
+   struct Payload
+   {
+      public Body[] Bodies { get; set; }
+
+      public BodyIndex BodyIndex { get; set; }
+
+      public Payload(Body[] bodies, BodyIndex bodyIndex)
+      {
+         Bodies = bodies;
+         BodyIndex = bodyIndex;
+      }
+   }
+
+   struct BodyIndex
+   {
+      public int Width { get; set; }
+
+      public int Height { get; set; }
+
+      [JsonConverter(typeof(ByteArrayConverter))]
+      public byte[] Pixels { get; set; }
+   }
+
+   class ByteArrayConverter : JsonConverter
+   {
+      public override bool CanConvert(Type objectType)
+      {
+         return objectType == typeof(byte[]);
+      }
+
+      public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+      {
+         if (value == null)
+         {
+            writer.WriteNull();
+            return;
+         }
+
+         writer.WriteStartArray();
+
+         var bytes = (byte[])value;
+
+         foreach (var @byte in bytes)
+            writer.WriteValue(@byte);
+
+         writer.WriteEndArray();
+      }
+
+      public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+      {
+         throw new NotImplementedException();
+      }
+   }
+
+}
---
+++ kinect-motion-server/WebSocketServer.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KinectMotion
+{
+   class WebSocketServer : IDisposable
+   {
+      public event EventHandler<ClientConnectionEventArgs> ClientConnected;
+
+      private HttpListener Listener { get; set; }
+
+      private IList<WebSocket> WebSockets { get; set; }
+
+      public const string Protocol = "KinectV2MotionV1";
+
+      public WebSocketServer(int port)
+      {
+         Listener = new HttpListener();
+         WebSockets = new List<WebSocket>();
+
+         // Bind only to one endpoint.
+         Listener.Prefixes.Add(string.Format("http://localhost:{0}/", port));
+      }
+
+      public void Start()
+      {
+         // Start listener and accept Http contexts.
+         Listener.Start();
+         Listener.BeginGetContext(new AsyncCallback(HandleGetContext), this);
+      }
+
+      public IEnumerable<string> Endpoints
+      {
+         get
+         {
+            return Listener.Prefixes;
+         }
+      }
+
+      public async Task Send(byte[] bytes)
+      {
+         var tasks = new List<Task>();
+         var segment = new ArraySegment<byte>(bytes);
+
+         lock (WebSockets)
+         {
+
+            // Send bytes to every client.
+            foreach (var webSocket in WebSockets)
+               tasks.Add(
+                  webSocket.SendAsync(segment, WebSocketMessageType.Binary, true, CancellationToken.None)
+
+                  // Ignore task result completely. This will allow some send requests
+                  // to throw an exception if for example the socket has died.
+                  .ContinueWith(x => Task.FromResult<object>(null)));
+         }
+
+         await Task.WhenAll(tasks);
+      }
+
+      public bool KeepAlive()
+      {
+         lock (WebSockets)
+         {
+
+            // Clean up all client connections.
+            for (int i = WebSockets.Count - 1; i >= 0; --i)
+            {
+
+               // Remove a connection if its state is closed, closing or aborted.
+               if (WebSockets[i].State == WebSocketState.Aborted || WebSockets[i].State == WebSocketState.Closed || WebSockets[i].State == WebSocketState.CloseReceived)
+                  WebSockets.RemoveAt(i);
+            }
+         }
+
+         // Tell if the server is still listening.
+         return Listener.IsListening;
+      }
+
+      public void Dispose()
+      {
+         var tasks = new List<Task>();
+
+         foreach (var webSocket in WebSockets)
+            tasks.Add(
+               webSocket.CloseAsync(WebSocketCloseStatus.Empty, "Server is shutting down.", CancellationToken.None)
+
+               // Ignore task result completely. This will allow some close requests
+               // to throw an exception if for example the socket has died.
+               .ContinueWith(_ => Task.FromResult<object>(null)));
+
+         // Wait for all sockets to close.
+         Task.WaitAll(tasks.ToArray());
+
+         // Stop Http listener.
+         Listener.Stop();
+      }
+
+      private void OnClientConnected(int connections)
+      {
+         ClientConnected?.Invoke(this, new ClientConnectionEventArgs(connections));
+      }
+
+      private async static Task<WebSocket> HandleWebSocketContext(HttpListenerContext context)
+      {
+         // Accept a WebSocket using a custom transfer protocol.
+         var webSocketContext = await context.AcceptWebSocketAsync(Protocol);
+
+         return webSocketContext.WebSocket;
+      }
+
+      private static void HandleHttpRequest(HttpListenerRequest request, HttpListenerResponse response)
+      {
+         // No regular Http requests are supported.
+         throw new HttpListenerException(400, "Web socket request was expected.");
+      }
+
+      private static void HandleGetContext(IAsyncResult result)
+      {
+         var server = (WebSocketServer)result.AsyncState;
+         var context = server.Listener.EndGetContext(result);
+
+         try
+         {
+            if (context.Request.IsWebSocketRequest)
+            {
+               var webSocket = HandleWebSocketContext(context).Result;
+
+               int connections;
+
+               // Store all connected clients. These are cleaned up in KeepAlive
+               // method.
+               lock (server.WebSockets)
+               {
+                  server.WebSockets.Add(webSocket);
+                  connections = server.WebSockets.Count;
+               }
+
+               server.OnClientConnected(connections);
+            }
+            else
+               HandleHttpRequest(context.Request, context.Response);
+         }
+         catch (HttpListenerException e)
+         {
+            var message = Encoding.UTF8.GetBytes(e.Message);
+
+            context.Response.StatusCode = e.ErrorCode;
+            context.Response.OutputStream.Write(message, 0, message.Length);
+            context.Response.Close();
+         }
+         catch (Exception e)
+         {
+            var message = Encoding.UTF8.GetBytes(e.Message);
+
+            context.Response.StatusCode = 500;
+            context.Response.OutputStream.Write(message, 0, message.Length);
+            context.Response.Close();
+         }
+
+         // Allow also other clients to connect.
+         server.Listener.BeginGetContext(new AsyncCallback(HandleGetContext), server);
+      }
+   }
+
+   class ClientConnectionEventArgs : EventArgs
+   {
+      public int Connections { get; private set; }
+
+      public ClientConnectionEventArgs(int connections)
+      {
+         Connections = connections;
+      }
+   }
+}
